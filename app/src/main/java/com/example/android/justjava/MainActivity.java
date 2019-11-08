a435d91a1593275656fae95d7b03eaaaf1d37bcc
cristian <cristiandavidgomezmunoz@gmail.com>
2019-11-08 18:00:23 -0500

    Fix buttons

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,6 +22,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 5;
-        display(quantity);
         displayPrice(quantity * 5);
     }
 
@@ -51,5 +51,21 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method increments the quantity on the screen.
+     */
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    /**
+     * This method decrements the quantity on the screen.
+     */
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        display(quantity);
     }
 }
